7c7a77abbe570aabcac8e4594172050ee46e470b
BennettIronYard <bennettiy12@gmail.com>
2015-10-09 11:20:57 -0400

    Running loop

--- src/Game.java
+++ src/Game.java
@@ -7,10 +7,12 @@ public class Game {
     public static void run() throws Exception {
         System.out.println("Welcome to my text adventure.");
 
+    while (true) {
         Player player = new Player();
         player.chooseName();
         player.chooseWeapon();
         player.chooseArea();
+        }
     }
     static String nextLine() {
         Scanner scanner = new Scanner(System.in);
@@ -19,6 +21,8 @@ public class Game {
             if (s.equals ("help")) {
                 System.out.println("These are the available commands:");
                 System.out.println("help => List available commands.");
+            }else if (s.equals("/exit")) {
+                System.exit(0);
             }
             return nextLine();
         }else {
