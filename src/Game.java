9e58e672de79f931c6fc7d93fc0137e9a3aff665
BennettIronYard <bennettiy12@gmail.com>
2015-10-14 12:25:18 -0400

    Save with json

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,9 @@
+import jodd.json.JsonParser;
+import jodd.json.JsonSerializer;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -9,32 +15,35 @@ public class Game {
 
     public static void main(String [] args) throws Exception {
         System.out.println("Welcome to my text adventure.");
-        player = new Player();
+        player = loadGame();
 
-    while (true) {
-        player.chooseName();
-        player.chooseWeapon();
-        player.chooseArea();
-        player.findItem("Shield");
+        if (player == null) {
+            player = new Player();
+            player.chooseName();
+            player.chooseWeapon();
+            player.chooseArea();
+            player.findItem("Shield");
+        }
 
         Weapon ogreWeapon = new Weapon();
         ogreWeapon.name = "Club";
         ogreWeapon.damage = 5;
         Enemy ogre = new Enemy("Ogre", 50, 5, ogreWeapon);
         player.battle(ogre);
-        }
+
+        saveGame();
     }
 
     static String nextLine() {
         Scanner scanner = new Scanner(System.in);
         String s = scanner.nextLine();
         if (s.startsWith("/")) {
-            if (s.equals ("help")) {
+            if (s.equals("help")) {
                 System.out.println("These are the available commands:");
                 System.out.println("help => List available commands.");
-            }else if (s.equals("/exit")) {
+            } else if (s.equals("/exit")) {
                 System.exit(0);
-            }else if (s.equals("/inv")) {
+            } else if (s.equals("/inv")) {
                 if (player.items.size() == 0) {
                     System.out.println("You have no items");
                 }
@@ -43,8 +52,37 @@ public class Game {
                 }
             }
             return nextLine();
-        }else {
+        } else {
             return s;
         }
+    }
+
+    static void saveGame() {
+        File f = new File("Save.json");
+        JsonSerializer serializer = new JsonSerializer();
+        String contentsToSave = serializer.serialize(player);
+
+        try {
+            FileWriter fw = new FileWriter(f);
+            fw.write(contentsToSave);
+            fw.close();
+        } catch (Exception e) {
+
+        }
+    }
+
+    static Player loadGame() {
+     try {
+         File f = new File("Save.json");
+         FileReader fr = new FileReader(f);
+         int fileSize = (int) f.length();
+         char[] contents = new char[fileSize];
+         fr.read(contents);
+         String fileContents = new String(contents);
+         JsonParser parser = new JsonParser();
+         return parser.parse(fileContents, Player.class);
+     }catch (Exception e) {
+         return null;
+     }
     }
 }
